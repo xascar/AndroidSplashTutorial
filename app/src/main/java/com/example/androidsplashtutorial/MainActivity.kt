212170ca79e46e66371cab600f4d850d87afbfe6
Oscar Gustavo Moreno Bautista <xascar94@gmail.com>
2022-11-12 15:06:27 -0500

    Implementando splash screen

--- app/src/main/java/com/example/androidsplashtutorial/MainActivity.kt
+++ app/src/main/java/com/example/androidsplashtutorial/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.androidsplashtutorial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread.sleep(1000)
+        screenSplash.setKeepOnScreenCondition{ false }
+//        val intent = Intent(this, DetailActivity::class.java)
+//        startActivity(intent)
+//        finish()
+
+//        val content: View = findViewById(android.R.id.content)
+//        content.viewTreeObserver.addOnPreDrawListener(
+//            object: ViewTreeObserver.OnPreDrawListener{
+//                override fun onPreDraw(): Boolean {
+//                    // Check if the initial data is ready.
+//                    return if (viewModel.isReady){
+//                        //The content is ready; start drawing.
+//                        content.viewTreeObserver.removeOnPreDrawListener(this)
+//                        true
+//                    } else {
+//                        //The content is not ready; suspend.
+//                        false
+//                    }
+//                }
+//            }
+//        )
+
     }
 }
